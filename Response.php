271d9723fcdca540e2ad4a7e21d57c443df3b7a5
fayaazn <fnabie@roc-dev.com>
2018-06-02 19:27:54 +0200

    PHPCHALLENGE

--- Response.php
+++ Response.php
@@ -1,9 +1,12 @@
 <?php
-if (($_POST["gnaam"] =="2059934" && $_POST["wwoord"]=="Fayaaz") ||
-    ($_POST["gnaam"] =="2073636" && $_POST["wwoord"]=="Tim")
+if (($_GET["gnaam"] =="fayaaz" && $_GET["wwoord"]=="2059934") ||
+    //($_GET["gnaam"] =="tim" && $_GET["wwoord"]=="2073636") ||
+    ($_GET["gnaam"] =="faaziya" && $_GET["wwoord"]=="hoi")
     )
 {
-    echo "Welkom ".$_POST["gnaam"]. ".";
+    echo "Welkom ".$_GET["gnaam"]. ".";
+    print( '<a href="index2.php">Ga naar Home.</a>' );
+
 }else {
     echo "Sorry, Je bent niet welkom";
 
